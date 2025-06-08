0345752308c5ddd9136281329194e27b52473905
KennyAle <kennyviver@gmail.com>
2025-06-08 02:49:52 -0700

    FAQ

--- final-project/src/app/page.tsx
+++ final-project/src/app/page.tsx
@@ -1,10 +1,12 @@
+import Faq from "@/components/Faq";
 import Hero from "@/components/Hero";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>
+    <div className="bg-stone-300 dark:bg-neutral-900">
       <Hero />
+      <Faq />
     </div>
   );
 }
