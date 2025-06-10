13e5d617da13fff2d4dfa572f1fbfc99713f9023
Tomoya <flyingfox193807@gmail.com>
2025-06-10 10:01:28 -0700

    added add product button on products page

--- final-project/src/app/admin/products/page.tsx
+++ final-project/src/app/admin/products/page.tsx
@@ -37,39 +37,46 @@ const AdminProducts = () => {
 
 
   return (
-    <div className="m-6 flex flex-col items-center">
-      <h1 className="text-2xl mb-4">All Products</h1>
-      <div className="w-full">
-        {products.map((product: DummyProduct) => (
-          <div key={product.id} className={`flex justify-between items-stretch ${product.id === 1 ? 'border' : 'border-x border-b'}`}>
-            <div className="relative h-[150px] w-[150px] border-r">
-              <Image src={product.thumbnail} alt={product.title} fill className="object-contain" /> 
-            </div>
-            <div className="flex flex-col justify-center">
-              <div className="flex gap-10 mb-2">
-                <h3 className="text-xl line-clamp-2">{product.title}</h3>
-                <p>Category: {product.category}</p>
-              </div>
-              <p className="max-w-[600px] line-clamp-2 mb-4 text-sm">{product.description}</p>
-              <p className="text-lg">Price: ${product.price}</p>
-            </div>
-            <div className="flex justify-center">
-              <div className="flex items-center border-l px-4">
-                <Link href={`/admin/products/${product.id}`}>
-                  <button className="border rounded-lg py-2 px-4 cursor-pointer hover:bg-gray-200 transition">Detail</button>
-                </Link>
-              </div>
-              <div className="flex items-center border-x px-4">
-                <Link href={`/admin/edit-product/${product.id}`}>
-                  <button className="border rounded-lg py-2 px-4 hover:bg-gray-200 transition">Edit</button>
-                </Link>
-              </div>
-              <div className="flex items-center px-4">
-                <button className="border rounded-lg py-2 px-4 bg-red-500 text-black hover:bg-red-300 transition">Delete</button>
-              </div>
-            </div>
+    <div className="flex flex-col w-full items-end">
+      <div className="m-6 w-3/4">
+        <div className="text-center mb-10 w-full flex  items-center justify-end gap-4">
+          <div className="w-full flex-1">
+            <h1 className="text-2xl">All Products</h1>
           </div>
-        ))}
+          <Link href='/admin/add-product' className="shadow-[0_0_1px] rounded-lg px-4 py-3 mr-10 hover:bg-gray-100 transition w-32">Add Product</Link>
+        </div>
+        <div className="w-full">
+          {products.map((product: DummyProduct) => (
+            <div key={product.id} className={`flex justify-between items-stretch ${product.id === 1 ? 'border' : 'border-x border-b'}`}>
+              <div className="relative h-[150px] w-[150px] border-r">
+                <Image src={product.thumbnail} alt={product.title} fill className="object-contain" /> 
+              </div>
+              <div className="flex flex-col justify-center">
+                <div className="flex justify-between mb-2">
+                  <h3 className="text-xl line-clamp-2 max-w-[230px]">{product.title}</h3>
+                  <p>Category: {product.category}</p>
+                </div>
+                <p className="max-w-[400px] line-clamp-2 mb-4 text-sm">{product.description}</p>
+                <p className="text-lg">Price: ${product.price}</p>
+              </div>
+              <div className="flex justify-center">
+                <div className="flex items-center border-l px-4">
+                  <Link href={`/admin/products/${product.id}`}>
+                    <button className="border rounded-lg py-2 px-4 cursor-pointer hover:bg-gray-200 transition">Detail</button>
+                  </Link>
+                </div>
+                <div className="flex items-center border-x px-4">
+                  <Link href={`/admin/edit-product/${product.id}`}>
+                    <button className="border rounded-lg py-2 px-4 hover:bg-gray-200 transition">Edit</button>
+                  </Link>
+                </div>
+                <div className="flex items-center px-4">
+                  <button className="border rounded-lg py-2 px-4 bg-red-500 text-black hover:bg-red-300 transition">Delete</button>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   )
