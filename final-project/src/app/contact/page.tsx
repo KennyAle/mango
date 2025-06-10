2e7bf41b1ec9069d205e28b2fa34b7ad0b90993a
Tomoya <flyingfox193807@gmail.com>
2025-06-10 12:07:00 -0700

    added emailjs

--- final-project/src/app/contact/page.tsx
+++ final-project/src/app/contact/page.tsx
@@ -1,9 +1,34 @@
+'use client'
+
+import emailjs from '@emailjs/browser'
+import { FormEvent, useRef } from 'react'
+
+
 const Contact = () => {
+
+  const formRef = useRef<HTMLFormElement>(null)
+
+
+  const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+
+    if (!formRef.current) return;
+
+    try {
+      await emailjs.sendForm('service_53bu84b', 'template_m600t96', formRef.current, 'nTWPDs-UosTwE2uZH')
+      alert('message sent successfully')
+      formRef.current.reset()
+    } catch(err) {
+      console.log(err)
+      alert('failed to send message')
+    }
+  }
+  
   return (
     <div className="flex flex-col justify-center items-center w-full min-h-screen">
       <div className="bg-white dark:bg-neutral-800 p-8 w-4/5 md:w-2/4">
         <h1 className="text-center text-2xl mb-4">Contact</h1>
-        <form className="flex flex-col gap-4">
+        <form onSubmit={handleSubmit} ref={formRef} className="flex flex-col gap-4">
           <div className="flex flex-col">
             <label htmlFor="username">Username</label>
             <input type="text" name="username" placeholder="Enter your username..." className="border p-3 rounded bg-white dark:bg-neutral-700 dark:border-neutral-600 text-black dark:text-white" />
