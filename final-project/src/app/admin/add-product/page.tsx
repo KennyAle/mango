1ef26823eceaa5b3208d87ffe03c386d3d918cec
Tomoya <flyingfox193807@gmail.com>
2025-06-10 12:30:52 -0700

    modified add product page style

--- final-project/src/app/admin/add-product/page.tsx
+++ final-project/src/app/admin/add-product/page.tsx
@@ -87,30 +87,40 @@ const AddProduct = () => {
   }, [products])
   
   return (
-    <div className="flex flex-col justify-center items-center p-4">
-      <h1 className="text-xl mb-4">Add Product</h1>
-      <form onSubmit={handleSubmit} className="flex flex-col w-2/3 bg-gray-100 rounded-lg p-8">
-        <label htmlFor="title">Product Title</label>
-        <input type="text" name="title" required placeholder="Enter product title..." value={formInputs.title} onChange={(e) => handleChange(e)} className="border p-1 rounded mb-4 mt-1" />
-        <label htmlFor="image">Product Image</label>
-        <input type="file" name="image" onChange={(e) => handleImage(e)} className="border p-1 rounded mb-4 mt-1" />
-        <label htmlFor="description">Product Description</label>
-        <textarea name="description" id="description" required placeholder="Enter product description..." value={formInputs.description} onChange={(e) => handleChange(e)} className="border p-1 rounded mb-4 mt-1"></textarea>
-        <label htmlFor="price">Product Price</label>
-        <input type="number" name="price" required placeholder="Enter product price..." value={formInputs.price} onChange={(e) => handleChange(e)} className="border p-1 rounded mb-4 mt-1" />
-        <div>
-          <h3>Product Category</h3>
+    <div className="w-full min-h-screen flex justify-center items-center p-10">
+      <div className="flex flex-col justify-center items-center p-6 w-2/3 bg-white dark:bg-neutral-800">
+        <h1 className="text-xl">Add Product</h1>
+        <form onSubmit={handleSubmit} className="flex flex-col w-full rounded-lg p-8 gap-4">
           <div className="flex flex-col">
-            {categoriesList.map((cat, index) => (
-              <label key={index} className="pl-2">
-                <input type="checkbox" value={cat} checked={formInputs.category?.includes(cat)} onChange={(e) => handleCategory(e)} />
-                <span>{cat}</span>
-              </label>
-            ))}
+            <label htmlFor="title">Product Title</label>
+            <input type="text" name="title" required placeholder="Enter product title..." value={formInputs.title} onChange={(e) => handleChange(e)} className="border p-3 rounded bg-white dark:bg-neutral-700 dark:border-neutral-600 text-black dark:text-white" />
           </div>
-        </div>
-        <button className="border rounded p-2 mt-4">Submit</button>
-      </form>
+          <div className="flex flex-col">
+            <label htmlFor="image">Product Image</label>
+            <input type="file" name="image" onChange={(e) => handleImage(e)} className="border p-3 rounded bg-white dark:bg-neutral-700 dark:border-neutral-600 text-black dark:text-white" />
+          </div>
+          <div className="flex flex-col">
+            <label htmlFor="description">Product Description</label>
+            <textarea name="description" id="description" required placeholder="Enter product description..." value={formInputs.description} onChange={(e) => handleChange(e)} className="border p-3 rounded bg-white dark:bg-neutral-700 dark:border-neutral-600 text-black dark:text-white"></textarea>
+          </div>
+          <div className="flex flex-col">
+            <label htmlFor="price">Product Price</label>
+            <input type="number" name="price" required placeholder="Enter product price..." value={formInputs.price} onChange={(e) => handleChange(e)} className="border p-3 rounded bg-white dark:bg-neutral-700 dark:border-neutral-600 text-black dark:text-white" />
+          </div>
+          <div>
+            <h3>Product Category</h3>
+            <div className="flex flex-col">
+              {categoriesList.map((cat, index) => (
+                <label key={index} className="pl-2">
+                  <input type="checkbox" value={cat} checked={formInputs.category?.includes(cat)} onChange={(e) => handleCategory(e)} />
+                  <span>{cat}</span>
+                </label>
+              ))}
+            </div>
+          </div>
+          <button className="bg-black text-white p-3 text-sm uppercase hover:bg-gray-900/80 transition">Submit</button>
+        </form>
+      </div>
     </div>
   )
 }
